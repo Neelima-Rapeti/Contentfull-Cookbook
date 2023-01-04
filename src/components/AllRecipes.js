9bb5dd2a52107eaa22e7a0374102ad2bc6502f68
cele <cele@cele.com>
2023-01-04 15:37:01 +0100

    fetch data

--- src/components/AllRecipes.js
+++ src/components/AllRecipes.js
@@ -1,5 +1,65 @@
 import React from "react";
+import { useState, useEffect } from "react";
+import * as contentful from "contentful";
 
 export default function AllRecipes() {
-  return <div>AllRecipes</div>;
+  const [recipe, setRecipe] = useState([]);
+
+  useEffect(() => {
+    const client = contentful.createClient({
+      space: process.env.REACT_APP_SPACE,
+      accessToken: process.env.REACT_APP_ACCESSTOKEN,
+    });
+
+    client
+      .getEntries()
+      .then((response) => setRecipe(response.items))
+      .catch(console.error);
+  }, []);
+  return (
+    <div>
+      <h3>AllRecipes</h3>
+      <div>
+        {recipe.map((rec) => {
+          return (
+            <div key={rec.sys.id} className="border m-5 ">
+              <p> {rec.fields.title}</p>
+              <p>yield: {rec.fields.metrics[0]}</p>
+              <p>prep: {rec.fields.metrics[1]}</p>
+              <p>cook: {rec.fields.metrics[2]}</p>
+              <p>total: {rec.fields.metrics[3]}</p>
+              <p> {rec.fields.description}</p>
+              <p>rating: {rec.fields.rating}</p>
+              <div>
+                <p>INGREDIENTS </p>
+                <ul>
+                  {rec.fields.ingredients.map((el, index) => (
+                    <li key={index}>{el}</li>
+                  ))}
+                </ul>
+              </div>
+              <div>
+                <p>INSTRUCTIONS</p>
+
+                <ol>
+                  {rec.fields.instructions.map((el, index) => (
+                    <li key={index}>{el}</li>
+                  ))}
+                </ol>
+              </div>
+              <div>
+                {
+                  <img
+                    src={rec.fields.image.fields.file.url}
+                    alt={rec.fields.title}
+                    className="w-25"
+                  />
+                }
+              </div>
+            </div>
+          );
+        })}
+      </div>
+    </div>
+  );
 }
