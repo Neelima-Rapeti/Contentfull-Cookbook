51ed7b798e32c0a2958640e2c8ff9655c7af70e7
cele <cele@cele.com>
2023-01-04 11:48:21 +0100

    add basic structure

--- src/App.js
+++ src/App.js
@@ -1,9 +1,20 @@
 import "./App.css";
+import Navbar from "./components/Navbar";
+import Homepage from "./components/Homepage";
+import AllRecipes from "./components/AllRecipes";
+import IndividualRecipe from "./components/IndividualRecipe";
+import Footer from "./components/Footer";
+import "bootstrap/dist/css/bootstrap.min.css";
+//https://react-icons.github.io/react-icons
 
 function App() {
   return (
     <div className="App">
-      <h1>Hello </h1>
+      <Navbar />
+      <Homepage />
+      <AllRecipes />
+      <IndividualRecipe />
+      <Footer />
     </div>
   );
 }
