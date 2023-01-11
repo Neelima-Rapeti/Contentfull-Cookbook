b48244cb80579d90345c8dca504dec36f278f433
Ana Rosa <asofiamcarvalho@gmail.com>
2023-01-11 15:52:58 +0100

    add allRecipes page

--- src/components/AllRecipes.js
+++ src/components/AllRecipes.js
@@ -3,74 +3,141 @@ import { useState, useEffect } from "react";
 import { NavLink } from "react-router-dom";
 import { Button } from "react-bootstrap";
 import client from "../client";
+import eye from '../images/eye.gif';
+import Container from 'react-bootstrap/Container';
+import Nav from 'react-bootstrap/Nav';
+import Navbar from 'react-bootstrap/Navbar';
+import NavDropdown from 'react-bootstrap/NavDropdown';
+
 export default function AllRecipes() {
   const [recipe, setRecipe] = useState([]);
 
   useEffect(() => {
     client
       .getEntries({
-        content_type: "blog",
+        
+        content_type: 'blog',
+  
       })
       .then((response) => setRecipe(response.items))
       .catch(console.error);
   }, []);
-  console.log(recipe);
+  // console.log(recipe);
+
+  const [isHovering, setIsHovering] = useState(false);
+
+  const handleMouseOver = () => {
+    setIsHovering(true);
+  }
+
+  const handleMouseOut = () => {
+    setIsHovering(false);
+  }
+
   return (
     <div>
-      <h3>AllRecipes</h3>
-      <NavLink to="/">Homepage</NavLink>
       <div>
-        <div className="container">
-          <div className="row">
+      <Navbar bg="light" expand="lg">
+      <Container className="d-flex justify-content-between">
+      
+        <Navbar.Brand><h2>All Recipes</h2></Navbar.Brand>
+        
+        <div>
+        <Navbar.Collapse id="basic-navbar-nav">
+          <Nav className="me-auto ">
+          <Nav.Link href="/">Home</Nav.Link>
+            <Nav.Link href="#link">Link</Nav.Link>
+
+            <NavDropdown title="Ingredients" id="basic-nav-dropdown" >
+              <div className="px-2">
+            <div class="form-check">
+                <input type="checkbox" class="form-check-input" id="exampleCheck1"/>
+               <label class="form-check-label" for="exampleCheck1">chicken</label>
+              </div>
+              
+                <div class="form-check">
+                <input type="checkbox" class="form-check-input" id="exampleCheck1"/>
+               <label class="form-check-label" for="exampleCheck1">tomatoes</label>
+              </div>
+             
+              
+                <div class="form-check">
+                <input type="checkbox" class="form-check-input" id="exampleCheck1"/>
+               <label class="form-check-label" for="exampleCheck1">mushrooms</label>
+              </div>
+             
+              
+                <div class="form-check">
+                <input type="checkbox" class="form-check-input" id="exampleCheck1"/>
+               <label class="form-check-label" for="exampleCheck1">pasta</label>
+              </div>
+              
+                <div class="form-check">
+                <input type="checkbox" class="form-check-input" id="exampleCheck1"/>
+               <label class="form-check-label" for="exampleCheck1">asparagus</label>
+              </div>
+              
+                <div class="form-check">
+                <input type="checkbox" class="form-check-input" id="exampleCheck1"/>
+               <label class="form-check-label" for="exampleCheck1">shallots</label>
+              </div>
+              <div className="my-2 mx-5">
+              <button type="submit" class="btn btn-warning">Submit</button>
+              </div>
+              </div>
+            </NavDropdown>
+            
+            <NavDropdown title="Order by" id="basic-nav-dropdown">
+              <NavDropdown.Item href="/allrecipes/categories/chicken">Most recent</NavDropdown.Item>
+              <NavDropdown.Item href="/allrecipes/bestrated">
+                Best rated
+              </NavDropdown.Item>
+              <NavDropdown.Item href="/allrecipes/preparationtime">Preparation time</NavDropdown.Item>
+              <NavDropdown.Divider />
+              <NavDropdown.Item href="/allrecipes/separatelink">
+                A-Z
+              </NavDropdown.Item>
+            </NavDropdown>
+
+            <NavDropdown title="Categories" id="basic-nav-dropdown">
+              <NavDropdown.Item href="/allrecipes/categories/chicken">Chicken</NavDropdown.Item>
+              <NavDropdown.Item href="/allrecipes/bestrated">
+                Best rated
+              </NavDropdown.Item>
+              <NavDropdown.Item href="/allrecipes/preparationtime">Mushrooms</NavDropdown.Item>
+              <NavDropdown.Divider />
+              <NavDropdown.Item href="/allrecipes/separatelink">
+                Separated link
+              </NavDropdown.Item>
+            </NavDropdown>
+
+
+
+          </Nav>
+        </Navbar.Collapse>
+        </div>
+      </Container>
+    </Navbar>
+      </div>
+      
+      <div className="grid mx-5" >
             {recipe.map((rec) => {
               return (
-                <div
-                  key={rec.sys.id}
-                  className="border col-sm-4 col-6 mx-auto my-4 "
-                >
-                  <p> {rec.fields.title}</p>
-                  <p>yield: {rec.fields.metrics[0]}</p>
-                  <p>prep: {rec.fields.metrics[1]}</p>
-                  <p>cook: {rec.fields.metrics[2]}</p>
-                  <p>total: {rec.fields.metrics[3]}</p>
-                  <p> description: {rec.fields.description}</p>
-                  <p>rating: {rec.fields.rating}</p>
-                  <p></p>
-                  <div>
-                    <p>INGREDIENTS </p>
-                    <ul>
-                      {rec.fields.ingredients.map((el, index) => (
-                        <li key={index}>{el}</li>
-                      ))}
-                    </ul>
-                  </div>
-                  <div>
-                    <p>INSTRUCTIONS</p>
-
-                    <ol>
-                      {rec.fields.instructions.map((el, index) => (
-                        <li key={index}>{el}</li>
-                      ))}
-                    </ol>
-                  </div>
-                  <div>
-                    {
-                      <img
-                        src={rec.fields.image.fields.file.url}
-                        alt={rec.fields.title}
-                        className="w-25"
-                      />
-                    }
-                  </div>
-                  <NavLink to={`/recipe/${rec.sys.id}`}>
-                    <Button variant="primary">View Details</Button>
+                <div className="cell" onMouseOver={handleMouseOver} onMouseOut={handleMouseOut}>
+                <img className="foto" src={rec.fields.image.fields.file.url} alt={rec.fields.title} />
+                  <p>{rec.fields.title.toLowerCase().charAt(0).toUpperCase() + rec.fields.title.toLowerCase().slice(1)}</p>
+                  
+                    {isHovering && (  
+                  <div className="eye" >
+                  <NavLink className="eyeNavlink" to={`/recipe/${rec.sys.id}`} >
+                    <img className="eyeGif" src={eye} alt="see more"/>
                   </NavLink>
-                </div>
-              );
+                  </div>
+                  )}   
+                  
+                  </div> );
             })}
+          </div> 
           </div>
-        </div>
-      </div>
-    </div>
   );
 }
